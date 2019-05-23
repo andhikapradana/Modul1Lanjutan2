40dd13a41f853bc367eeb1a96b783e7e7a51b1eb
andhikapradana <andhika_pradana_26rpl@student.smktelkom-mlg.sch.id>
2019-05-23 13:28:05 +0700

    Lanjutan 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# AndroidStudioLat2
+![alt text](https://github.com/DannyBramantyo9/AndroidStudioLat2/blob/master/2.png) <br>
+![alt text](https://github.com/DannyBramantyo9/AndroidStudioLat2/blob/master/3.png) <br>
---
+++ app/src/main/java/com/example/dhikaaa/lat2/Splash_screen.java
@@ -0,0 +1,40 @@
+package com.example.brammax.lat2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Splash_screen extends AppCompatActivity {
+    Thread ThreadSplash;
+    TextView txtTitle;
+    private long ms = 0;
+    private long splashTime = 3000;
+    private boolean splashActive = true;
+    private boolean splashPause = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread th = new Thread() {
+            public void run() {
+                try {
+                    while (splashActive && ms < splashTime) {
+                        if (!splashPause) {
+                            ms = ms + 100;
+                        }
+                        sleep(100);
+                    }
+                } catch (Exception e) {
+                    //TODO : handle exception
+                } finally {
+                    Intent i = new Intent(Splash_screen.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        th.start();
+    }
+}
